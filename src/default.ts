2c17682ff38c57cfbb469cb6f2dd044f2dc245b2
('3') <mozisan@users.noreply.github.com>
2024-09-17 03:29:34 +0900

    fix(prettier-config): better monorepo support

--- src/default.ts
+++ src/default.ts
@@ -1,5 +1,12 @@
+import { createRequire } from "node:module";
+
 import type { Config } from "prettier";
 
+const require = createRequire(import.meta.url);
+
 export default {
-	plugins: ["prettier-plugin-packagejson", "prettier-plugin-toml"],
+	plugins: [
+		require.resolve("prettier-plugin-packagejson"),
+		require.resolve("prettier-plugin-toml"),
+	],
 } satisfies Config;
--- src/tailwindcss.ts
+++ src/tailwindcss.ts
@@ -1,13 +1,17 @@
+import { createRequire } from "node:module";
+
 import type { Config } from "prettier";
 import type { PluginOptions } from "prettier-plugin-tailwindcss";
 
 import config from "./default.js";
 
+const require = createRequire(import.meta.url);
+
 const { plugins, ...defaultOptions } = config;
 
 export default {
 	...defaultOptions,
-	plugins: [...plugins, "prettier-plugin-tailwindcss"],
+	plugins: [...plugins, require.resolve("prettier-plugin-tailwindcss")],
 	tailwindFunctions: [
 		"classnames",
 		"clsx",
