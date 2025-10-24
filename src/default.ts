15b0af7ebc5e75a6d0bda681733d651a795fbb19
('3') <mozisan@users.noreply.github.com>
2025-10-25 03:09:33 +0900

    revert: disable experimentalTernaries

--- src/default.ts
+++ src/default.ts
@@ -1,7 +1,6 @@
 import type { Config } from "prettier";
 
 export default {
-	experimentalTernaries: true,
 	experimentalOperatorPosition: "start",
 	plugins: [
 		new URL(import.meta.resolve("prettier-plugin-packagejson")).pathname,
